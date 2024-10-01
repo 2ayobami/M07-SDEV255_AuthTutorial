94e3dde0956868fc8568bf781cf7108e62cd013b
Ayobami Oluwadeyi <ayobamifiyibomi@gmail.com>
2024-10-01 21:25:52 +0100

    Login sytem fully functional

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,20 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 // Handling errors
 const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = { email: '', password: '' };
+
+    // Incorrect email
+    if (err.message === 'Incorrect email') {
+        errors.email = 'That email is not registered';
+    }
+
+    // Incorrect password
+    if (err.message === 'Incorrect password') {
+        errors.password = 'That password is incorrect';
+    }
 
     // Duplicate email error code
     if (err.code === 11000) {
@@ -21,6 +32,13 @@ const handleErrors = (err) => {
     return errors;
 }
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+    return jwt.sign({ id }, 'SDEV255', {
+        expiresIn: maxAge
+    });
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -34,7 +52,9 @@ module.exports.signup_post = async (req, res) => {
 
     try {
         const user = await User.create({ email, password });
-        res.status(201).json(user);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: 1000 * maxAge });
+        res.status(201).json({ user: user._id });
     }
     catch (err) {
         const errors = handleErrors(err);
@@ -44,6 +64,20 @@ module.exports.signup_post = async (req, res) => {
 
 module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
-    console.log(email, password);
-    res.send('user login');
+    
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: 1000 * maxAge });
+        res.status(200).json({ user: user._id });
+    }
+    catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
+    }
+}
+
+module.exports.logout_get = (req, res) => {
+    res.cookie('jwt', '', { maxAge: 1 });
+    res.redirect('/');
 }
